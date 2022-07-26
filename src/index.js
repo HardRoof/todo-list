d6e969bacbaf0345a90b425c280eb31e7f174b4e
HardRoof <mateus_grandez@hotmail.com>
2022-07-25 18:49:36 -0600

    Updated a module name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './modules/style.css';
 import {modalBox} from './modules/modal.js';
-import createNotes from './modules/note.js';
+import createNotes from './modules/switchForm.js';
 
 
 modalBox();
