1e52411ff768cc72199fff4b11dd9fb3201e61a3
HardRoof <mateus_grandez@hotmail.com>
2022-07-28 23:17:56 -0600

    Updated main function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import './modules/style.css';
 import {modalBox} from './modules/modal.js';
 import createNotes from './modules/switchForm.js';
+import {listeners} from './modules/listeners.js';
+
 
 
 modalBox();
 createNotes();
+listeners();
 
 
+
+
+if (window.history.replaceState) {
+  window.history.replaceState(null, null, window.location.href);
+}
 
 
