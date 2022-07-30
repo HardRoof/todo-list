aed85a3edd08b7ba80d02e188340b4798928940c
HardRoof <mateus_grandez@hotmail.com>
2022-07-29 23:50:44 -0600

    Updated logic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import './modules/style.css';
 import {modalBox} from './modules/modal.js';
-import createNotes from './modules/switchForm.js';
 import {listeners} from './modules/listeners.js';
 
 
-
 modalBox();
-createNotes();
 listeners();
-
-
 
 
 if (window.history.replaceState) {
