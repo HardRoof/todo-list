cf8308c66d697106f0bf29199f1412d86fce0dd9
HardRoof <mateus_grandez@hotmail.com>
2022-07-29 23:53:49 -0600

    Added all listeners to this module

--- src/modules/listeners.js
+++ src/modules/listeners.js
@@ -1,24 +1,44 @@
-import {generatesToDoItem, clearForm, toDoItem} from './mainForm.js';
-import {showContent, showCheck} from './showContent.js';
+import {generatesToDoItem, clearForm} from './mainForm.js';
+import {showContent} from './showContent.js';
+import {dynamicDescription} from './dynamicDOM.js';
 
+const form = document.querySelector("#submitForm");
+const formNote = document.querySelector("#noteForm");
 
 function listeners() {
   document.getElementById('submitForm').addEventListener("submit", formAction);
-  document.getElementById('LibContainer').addEventListener("click", showCheck);
+  document.getElementsByTagName("li")[4].addEventListener("click", showMainForm);
+  document.getElementsByTagName("li")[5].addEventListener("click", showNoteForm);
+  document.getElementById('LibContainer').addEventListener("click", listActions);
 }
 
 function formAction(e) {
   e.preventDefault();
   generatesToDoItem();
   showContent();
-  
-  clearForm(generatesToDoItem())
+  generatesToDoItem().assignFlag();
+  clearForm();
 }
 
+function showMainForm() {
+  form.style.display = "grid";
+  formNote.style.display = "none";
+}
+  
+function showNoteForm() {
+  form.style.display = "none";
+  formNote.style.display = "flex";
+}
 
-// function listeners() {
-//   document.getElementById('submitForm').addEventListener("submit", generatesToDoItem)
-//   document.getElementById('submitForm').addEventListener("submit", showContent)
-// }
+function listActions(e) {
+  // console.log(e.target);
+  if (e.target.classList.contains('unchecked')) e.target.classList.toggle("checked");
+  if (e.target.classList.contains('trashBinSvg')) {
+    let grandParent = e.target.parentElement.parentElement;
+    grandParent.remove();
+  }
+  if (e.target.classList.contains('details')) dynamicDescription(e);
+}
+
 
 export {listeners}
