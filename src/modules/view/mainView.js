9b629bbec4d28421e1ea36f2b1ef9a2aad3cd467
HardRoof <mateus_grandez@hotmail.com>
2022-08-03 19:12:53 -0600

    Added functions to add/delete, check/uncheck todos

--- src/modules/view/mainView.js
+++ src/modules/view/mainView.js
@@ -5,13 +5,39 @@ class View {
     let temp = document.getElementsByTagName("template")[0];
     let clon = temp.content.cloneNode(true);
     document.getElementById("LibContainer").append(clon);
-  }
-  
-  dynamicDescription(e) {
-    console.log(e.target);
-    //fires func. that creates dom elem.
-  }
-};
+  };
 
+  getTarget(e) {
+    const grandParent = e.target.parentElement.parentElement;
+    const collapsedDiv = e.target.parentElement.parentElement.nextSibling;
+    const DetailBtn = e.target.parentElement.firstElementChild;
+    return {grandParent, collapsedDiv, DetailBtn};
+  };
+
+  removeToDoDiv(e) {
+    this.getTarget(e).grandParent.remove();
+  };
+
+  checkBox(e) {
+    e.target.classList.toggle("checked");
+  };
+
+  clickDetails(e) {
+    if (this.getTarget(e).DetailBtn.classList.contains('showingDetails')) this.getTarget(e).DetailBtn.click()
+  };
+
+  dynamicDetails(e) {
+    const detailsContainer = document.createElement('div');
+    detailsContainer.classList.add('detailsContent');
+    e.target.classList.toggle("showingDetails"); //To add CSS style when btn is pressed
+
+    if (e.target.classList.contains('showingDetails')) {
+      this.getTarget(e).grandParent.insertAdjacentElement("afterend", detailsContainer);
+      this.getTarget(e).collapsedDiv.style.maxHeight = this.getTarget(e).collapsedDiv.scrollHeight + "rem";
+      detailsContainer.textContent = "Lorem";
+    }
+    else this.getTarget(e).collapsedDiv.remove();
+  };
+};
 
 export {View}
