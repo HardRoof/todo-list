8eae687780090726148a82aa52159b70acb66b5a
HardRoof <mateus_grandez@hotmail.com>
2022-08-03 19:16:41 -0600

    Updated controller with new functions

--- src/modules/controller/mainController.js
+++ src/modules/controller/mainController.js
@@ -8,56 +8,63 @@ class Controller {
     document.getElementById('LibContainer').addEventListener("click", (e) => this.listActions(e));
     document.querySelector('#imageButton').addEventListener('click', () => this.openModalBox());
     document.getElementsByClassName("close")[0].addEventListener('click', () => this.closeModalBox());
-    window.addEventListener("click", (e) => this.closeModalOutside(e))  
-  }
+    window.addEventListener("click", (e) => this.closeModalOutside(e));
+  };
 
   getForms() {
     const form = document.querySelector("#submitForm");
     const formNote = document.querySelector("#noteForm");
     return {form,formNote}
-  }
+  };
 
   formAction(e) {
     e.preventDefault();
-    this.model.generatesToDoItem();
     this.view.showContent();
-    this.model.generatesToDoItem().assignFlag();
+    let increment = this.model.counter()
+    let toDoItem = this.model.generatesToDoItem(increment);
+    toDoItem.assignFlag();
+    this.model.addDivID(increment);
     this.model.clearForm();
-  }
+  };
+
+  closeModalOutside(e) {
+    if (e.target == document.getElementById("myModal")) {
+      this.closeModalBox();
+    }
+  };
+
+  openModalBox() {
+    document.getElementById("myModal").style.display = "block";
+  };
+
+  closeModalBox() {
+    document.getElementById("myModal").style.display = "none";
+  };
 
   showMainForm() {
     this.getForms().form.style.display = "grid";
     this.getForms().formNote.style.display = "none";
-  }
+  };
     
   showNoteForm() {
     this.getForms().form.style.display = "none";
     this.getForms().formNote.style.display = "flex";
-  }
+  };
   
   listActions(e) {
-    if (e.target.classList.contains('unchecked')) e.target.classList.toggle("checked");
+console.log(this.model.toDos);
+    if (e.target.classList.contains('unchecked')) {
+      this.view.checkBox(e);
+      this.model.toDoStatus(this.view.getTarget(e).grandParent.id);
+    }
     if (e.target.classList.contains('trashBinSvg')) {
-      let grandParent = e.target.parentElement.parentElement;
-      grandParent.remove();
-    }
-    if (e.target.classList.contains('details')) this.view.dynamicDescription(e);
-  }
-
-  closeModalOutside(e) {
-    if (e.target == document.getElementById("myModal")) {
-      this.closeModalBox()
-    }
-  }
-
-  openModalBox() {
-    document.getElementById("myModal").style.display = "block";
-  }
-
-  closeModalBox() {
-    document.getElementById("myModal").style.display = "none";
-  }
-}
+      this.view.clickDetails(e);
+      this.view.removeToDoDiv(e);
+      this.model.removeToDoItem(e);
+    };
+    if (e.target.classList.contains('details')) this.view.dynamicDetails(e);
+  };
+};
 
 
 export {Controller}
