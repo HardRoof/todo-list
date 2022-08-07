75d32ff954f3d2f7779c06f8f1afdb16e86f6c98
HardRoof <mateus_grandez@hotmail.com>
2022-08-07 00:40:34 -0600

    Created noteList Class

---
+++ src/modules/model/noteList.js
@@ -0,0 +1,13 @@
+class noteList {
+  constructor(noteTitle, noteDescription) {
+    this.noteTitle = noteTitle;
+    this.noteDescription = noteDescription;
+  };
+  fillNote() {
+    const noteTitle = document.getElementsByClassName("noteClass");
+    noteTitle[noteTitle.length-1].firstElementChild.textContent = this.noteTitle;
+    noteTitle[noteTitle.length-1].lastElementChild.textContent = this.noteDescription;
+  };
+}
+
+export {noteList}
