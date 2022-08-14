1a76f93ea0f2ffc270691f1b818c3449c0c55398
HardRoof <mateus_grandez@hotmail.com>
2022-08-13 19:44:10 -0600

    Added func. to display projects and today's todo

--- src/modules/view/mainView.js
+++ src/modules/view/mainView.js
@@ -1,4 +1,5 @@
-import detailsView from './detailsView'
+import detailsView from './detailsView';
+import {format, parseISO} from "date-fns";
 
 class View {
   constructor() {
@@ -36,23 +37,49 @@ class View {
   checkBox(e) {
     e.target.classList.toggle("checked");
     e.target.nextElementSibling.classList.toggle("strikethrough");
+    this.getTarget(e).grandParent.classList.toggle("opaque");
   };
 
   clickDetails(e) {
-    if (this.getTarget(e).DetailBtn.classList.contains('showingDetails')) this.getTarget(e).DetailBtn.click()
+    if (this.getTarget(e).DetailBtn.classList.contains('showingDetails')) this.getTarget(e).DetailBtn.click();
   };
 
   dynamicDetails(e, array) {
-    e.target.classList.toggle("showingDetails"); //test
+    e.target.classList.toggle("showingDetails");
     if (e.target.classList.contains('showingDetails')) {
       const detailsContainer = this.detailsView.createDetailsDiv(this.getTarget(e).grandParent);
       this.detailsView.collapse(this.getTarget(e).collapsedDiv);
       let content = this.detailsView.createContent(detailsContainer);
       let inputs = this.detailsView.getInputs(this.getTarget(e).grandParent, array);
-      this.detailsView.setInputs(content,inputs)
-
+      this.detailsView.setInputs(content,inputs);
     }
     else this.getTarget(e).collapsedDiv.remove();
+  };
+
+  sidebarProject(collapsed) {
+    this.detailsView.collapse(collapsed);
+  };
+
+  displayProjects(projectTitle) {
+    let div = document.createElement('div');
+    document.getElementsByTagName("section")[0].append(div);
+    let p = document.createElement('p');
+    p.classList.add('projectItem');
+    p.textContent = projectTitle;
+    div.append(p);
+    
+    let span = document.createElement('span');
+    span.classList.add('close');
+    div.append(span);
+  };
+
+  showToday() {
+    const today = format(parseISO(new Date().toISOString()), 'MMM do'); 
+    document.querySelectorAll(".todo.shell").forEach(item => {
+      item.style.display = "flex";
+      if (today == item.dataset.date) item.style.display = "flex";
+      else item.style.display = "none";
+    });
   };
 };
 
